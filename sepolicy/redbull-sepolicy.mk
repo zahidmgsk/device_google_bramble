aa8be36867364629f6cc481b0a9f61fecbbdb9ad
jhenrique09 <jhsv09@gmail.com>
2022-02-07 11:45:13 -0300

    redbull: Include googlebattery sepolicy

--- sepolicy/redbull-sepolicy.mk
+++ sepolicy/redbull-sepolicy.mk
@@ -12,6 +12,7 @@ BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy/vendor/verizon
 # Pixel-wide sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powerstats
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/input
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/googlebattery
 
 # system_ext
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/redbull/sepolicy/system_ext/private
