21435de14afa310dea2753007c7fd9f6cec5f7b9
Michael Bestas <mkbestas@lineageos.org>
2022-11-03 19:04:59 +0200

    redbull: Include missing ConnectivityThermalPowerManager sepolicy
    Change-Id: I1e0f05159a6797251ed85360966aa45246005bbd

--- sepolicy/redbull-sepolicy.mk
+++ sepolicy/redbull-sepolicy.mk
@@ -13,6 +13,7 @@ BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy/vendor/verizon
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powerstats
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/input
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/googlebattery
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/connectivity_thermal_power_manager
 
 # system_ext
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/redbull/sepolicy/system_ext/private
