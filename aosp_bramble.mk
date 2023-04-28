99d012f44dc486382740508ded6d9f96307c9795
jhenrique09 <jhsv09@gmail.com>
2023-04-28 11:34:35 -0300

    bramble: Update fingerprint

--- aosp_bramble.mk
+++ aosp_bramble.mk
@@ -81,8 +81,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=bramble \
-    PRIVATE_BUILD_DESC="bramble-user 13 TQ2A.230305.008.C1 9619669 release-keys"
+    PRIVATE_BUILD_DESC="bramble-user 13 TQ2A.230405.003 9719927 release-keys"
 
-BUILD_FINGERPRINT := google/bramble/bramble:13/TQ2A.230305.008.C1/9619669:user/release-keys
+BUILD_FINGERPRINT := google/bramble/bramble:13/TQ2A.230405.003/9719927:user/release-keys
 
 $(call inherit-product, vendor/google/bramble/bramble-vendor.mk)
