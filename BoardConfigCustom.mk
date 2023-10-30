eb90e58b500881c15ed5bfd1183b9a17fec942fd
author zahidmgsk <zahidmgsk@gmail.com>
2023-10-30 18:25:04 +0000

    redbull: Bring up superior os and Add missing FCM entries
    *The following HALs in device manifest are not declared in FCM <= level 5:
    vendor.lineage.livedisplay@2.0::IPictureAdjustment/default
    INCOMPATIBLE
    
    stderr:ERROR: files are incompatible: The following instances are in the device manifest but not specified in framework compatibility matrix:
        vendor.lineage.livedisplay@2.0::IPictureAdjustment/default
    
    Signed-off-by: zahidmgsk <zahidmgsk@gmail.com>

--- BoardConfigCustom.mk
+++ BoardConfigCustom.mk
@@ -26,7 +26,7 @@ BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(filter $(BOOT_KERNEL_MODULES), $(K
 
 # Manifests
 DEVICE_MANIFEST_FILE += device/google/redbull/lineage_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/aosp/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/superior/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += hardware/st/nfc/aidl/nfc-service-default.xml
 
 # Needed for camera
